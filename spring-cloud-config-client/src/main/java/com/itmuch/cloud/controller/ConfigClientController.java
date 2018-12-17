520048ee0317465c775a68e9137645a1aabbcfe8
thesea <thisislov3@163.com>
2018-12-17 23:55:54 +0800

    learning spring cloud config

---
+++ spring-cloud-config-client/src/main/java/com/itmuch/cloud/controller/ConfigClientController.java
@@ -0,0 +1,18 @@
+package com.itmuch.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientController {
+
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/profile")
+    public String queryProfile(){
+
+        return this.profile;
+    }
+}
