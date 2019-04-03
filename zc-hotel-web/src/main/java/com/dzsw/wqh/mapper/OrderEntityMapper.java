a95561a8d704ce7c8c32b818b76f04eda8e8e2bc
thesea <thisislov3@163.com>
2019-04-03 20:39:37 +0800

    移除zc-hotel

--- zc-hotel-web/src/main/java/com/dzsw/wqh/mapper/OrderEntityMapper.java
+++
@@ -1,7 +0,0 @@
-package com.dzsw.wqh.mapper;
-
-import com.dzsw.wqh.model.OrderEntity;
-import com.dzsw.wqh.util.MyMapper;
-
-public interface OrderEntityMapper extends MyMapper<OrderEntity> {
-}
--- zc-hotel-web/src/main/java/com/dzsw/wqh/mapper/UserEntityMapper.java
+++
@@ -1,7 +0,0 @@
-package com.dzsw.wqh.mapper;
-
-import com.dzsw.wqh.model.UserEntity;
-import com.dzsw.wqh.util.MyMapper;
-
-public interface UserEntityMapper extends MyMapper<UserEntity> {
-}
